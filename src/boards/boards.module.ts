6d3cb03a77db0c10f2cf5220d79f2aecb8c88965
이충한 <chunghan.lee@mirero.co.kr>
2023-12-26 10:53:12 +0900

    feat. 인증된 유저만 정보 조회 권한

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,9 +4,10 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board.entity';
 import { BoardRepoisotry } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepoisotry],
 })
