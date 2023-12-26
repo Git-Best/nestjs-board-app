859ca3b6d41074d982b29a0380bc00addcc3c3d4
이충한 <chunghan.lee@mirero.co.kr>
2023-12-26 09:59:03 +0900

    feat. jwt 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,29 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './auth.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOneBy({ username: username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'logIn success';
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('logIn failed');
     }
